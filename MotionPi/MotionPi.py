7c827fb0aad0771cb5bafb0af4f44f0fe1ccceea
psavella <patrick.savella@entechconsulting.com>
2018-06-21 13:25:54 -0400

    Capture image

--- MotionPi/MotionPi.py
+++ MotionPi/MotionPi.py
@@ -24,63 +24,59 @@ def take_a_pic(date_time):
     img="0"
     try:
         img = ('/home/pi/MotionPi3/MotionPi/images/%s.jpeg' % str(date_time))
-	#camera.capture(img, resize(320,240))
 	camera = PiCamera()
 	camera.resolution = (640, 480)
 	camera.framerate = (24, 1)
 
         camera.capture(img)
-	
         camera.close()       
-        
     except:
 	logging.warning("Camera Close error")
 	camera.close()
     finally:
         return img 
 
-def detect_motion(date_time):
+def detect_motion():
     while True:
         i = GPIO.input(11)
         if i == 0:                 #When output from motion sensor is LOW
-        	date_time1 = datetime.datetime.now()	   
-		print("No motion", date_time1, i)
-            	sleep(.2)
+        	date_time = datetime.datetime.now()	   
+		print("No motion", date_time, i)
+            	sleep(.3)
         elif i == 1:               #When output from motion sensor is HIGH
             x=0
             while x<limit:
-                  date_time1 = datetime.datetime.now()
-		  print("Motion detected", date_time1, i)
+                  date_time = datetime.datetime.now()
+		  print("Motion detected", date_time, i)
+                  sleep(.1)
                   if i==1:
                          x+=1
                   else:
                          break
                   if x==limit:
                          print("taking a pic")
-                         pic = take_a_pic(date_time1)
+                         pic = take_a_pic(date_time)
+                         hopethisworks(pic, date_time)
+                         
                          while i ==1:
-                                i = GPIO.input(11)
-                                print("1")
-                         return pic
+                             print("im here", datetime.datetime.now())
+                             i = GPIO.input(11)
+			     sleep(.3)
+                         return 
                   i = GPIO.input(11)
-                  #sleep(.2)
-   
-if __name__ == "__main__":
-    while True:
-        date_time = datetime.datetime.now()
-	pic = detect_motion(date_time)
+
+def hopethisworks(pic, date_time):
 	if pic == "0":
 		#log messge
 		logging.warning("Bad pic")
 	else:
             try:
 		try:
-                	with open(pic, 'rb') as f:
-                   	     binPic = base64.b64encode(f.read())
-#	    	    print(binPic)
+                    with open(pic, 'rb') as f:
+                        binPic = base64.b64encode(f.read())
                 except:
-			logging.warning("did not read picture")
-			binpic="0"
+                    logging.warning("did not read picture")
+		    binpic="0"
 		
 	    	jsonData = formatJson(deviceId , sensorId, str(date_time), binPic)
 		if jsonData == "0":
@@ -94,6 +90,10 @@ if __name__ == "__main__":
             except:
 		logging.warning("error sending data main")
                 pass 
+   
+if __name__ == "__main__":
+    while True:
+        detect_motion()
         # log the error
         # If post data returns error save locally at somepoint
 
--- MotionPi/httpRequest.py
+++ MotionPi/httpRequest.py
@@ -18,7 +18,7 @@ def postData(json_Data):
 	
 		if r.status_code == requests.codes.ok:
 			print("ok")
-			print(str(r.status_code))
+                        print(str(r.status_code))
 			return r.status_code
 		else: 
 			print("not ok")
@@ -30,7 +30,7 @@ def postData(json_Data):
 
 def formatJson(piId,sensorId,dateTime, pic):
 	try:
-		#dateTime = datetime.datetime.fromtimestamp(dateTime).strftime('%Y-%m-%d %H:%M:%S')
+                #dateTime = datetime.datetime.fromtimestamp(dateTime).strftime('%Y-%m-%d %H:%M:%S')
 		json_data = {"deviceId":piId,"sensorId": sensorId,"event_occurred":dateTime,"img":pic}
 		#print json.dumps(json_data)	
 		return json_data
